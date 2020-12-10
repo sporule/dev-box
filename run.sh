a78b621815b37da6724a8dd0edfa1e9000e50b07
Hao <hao@sporule.com>
2020-12-10 19:59:24 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+function add_users(){
+  for name in ${USERS[@]}
+  do
+        adduser $name --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password --force-badname
+        echo "$name:$USERS_PASSWORD" | chpasswd
+  done
+}
+
+add_users &
+
+# Run Jupyter Hub if it is enabled
+if [ "$JUPYTER" = "1" ]; then (jupyterhub --port=8080 &) ; fi
+
+
 # Start SSH
 service ssh restart && bash
 
